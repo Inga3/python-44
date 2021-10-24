e6205187aa09066c541c2be9de313365a921d091
INGA <in.nik201229@gmail.com>
2021-10-24 19:32:46 +0300

    nNitial project version

---
+++ hello6.py
@@ -0,0 +1,9 @@
+v_str = input("введие текст: ")
+
+for x , y  in ("Uncle", "Comrad")  , ("Styopa" , "Inga" ) , ("uncle" , "comrad") :
+        v_str = v_str.replace(x,y)
+     
+v_str  = v_str.replace(x , y) 
+print (v_str) 
+
+
